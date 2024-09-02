22756aba58fefb4dc77c7b33019c8bc0f1c666e9
rudijetson <info@engineer.marketing>
2024-09-02 18:31:03 -0400

    updted package.json

--- src/App.js
+++ src/App.js
@@ -1,19 +1,36 @@
-import React, { useEffect, useState } from 'react';
+// frontend/src/App.js
+import React from 'react';
 import Resume from './pages/Resume';
 
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true };
+  }
+
+  componentDidCatch(error, errorInfo) {
+    console.log('Error caught by ErrorBoundary:', error, errorInfo);
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <h1>Something went wrong.</h1>;
+    }
+
+    return this.props.children;
+  }
+}
+
 function App() {
-  const [isLoaded, setIsLoaded] = useState(false);
-
-  useEffect(() => {
-    console.log('App component mounted');
-    setIsLoaded(true);
-
-    return () => {
-      console.log('App component will unmount');
-    };
-  }, []);
-
-  return isLoaded ? <Resume /> : <div>Loading...</div>;
+  return (
+    <ErrorBoundary>
+      <Resume />
+    </ErrorBoundary>
+  );
 }
 
 export default App;
